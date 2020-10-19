4ac6907820fccd1476c2b800b053e28c68c7ba3b
artemsan1 <72440270+artemsan1@users.noreply.github.com>
2020-10-19 18:26:37 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
+let personalMovievDB = {
+    count: {numberOfFilms},
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+let a = prompt('Один из последних просмотренных фильмов?'),
+    b = prompt('Один из последних просмотренных фильмов?'),
+    c = prompt('Один из последних просмотренных фильмов?'),
+    d = prompt('Один из последних просмотренных фильмов?');
+
+// personalMovievDB.movies = 
+
+console.log(a);
